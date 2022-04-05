95ef91b8eefbe975651286b0d6e0d19a048edc4e
Daniel9ad <daniel.ad.dad9@gmail.com>
2022-04-05 10:00:53 -0400

    mensaje1

---
+++ principal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    do {
+        cout << "Daniel Avendaño Duran" << endl;
+        cout << "Biografia" << endl;
+        cout << "Naci en Sucre, Chuquisaca" << endl;
+        cout << "Estudie en los colegios: Ovando, Bernardo Monteagudo, Mariano y Lucio Siles" << endl;
+        cout << "Sali bachiller en el colegio Lucio Siles" << endl;
+        cout << "Estudio Ing. en ciencias de la computacion" << endl;
+
+        cout << "1.- Imprimir de nuevo" << endl;
+        cout << "2.- Salir" << endl;
+        
+        cin >> n;
+        
+
+    } while (n==1);
+
+}
